f7ba0c3a6611ec6392c908c4be7c3f7faccdddaf
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-12-30 17:17:19 +0000

    airport with getters

--- Airport.cpp
+++ Airport.cpp
@@ -6,16 +6,32 @@
 
 using namespace std;
 
-Airport::Airport() {}
-
-Airport::Airport(const string &code) : code(code) {}
-
-Airport::Airport(const std::string &code, const std::string &name, const std::string &city, const std::string &country,
-                 double latitude, double longitude) : code(code), name(name), city(city), country(country),
-                                                      latitude(latitude), longitude(longitude) {}
+Airport::Airport(const string &code, const string &name, const string &city, const string &country, double latitude,
+                 double longitude) : code(code), name(name), city(city), country(country), latitude(latitude),
+                                     longitude(longitude) {}
 
 const string &Airport::getCode() const {
     return code;
+}
+
+const string &Airport::getName() const {
+    return name;
+}
+
+const string &Airport::getCity() const {
+    return city;
+}
+
+const string &Airport::getCountry() const {
+    return country;
+}
+
+double Airport::getLatitude() const {
+    return latitude;
+}
+
+double Airport::getLongitude() const {
+    return longitude;
 }
 
 bool Airport::operator==(const Airport &rhs) const {
--- Airport.h
+++ Airport.h
@@ -10,11 +10,21 @@
 
 class Airport {
 public:
-    Airport();
-    Airport(const std::string &code);
     Airport(const std::string &code, const std::string &name, const std::string &city, const std::string &country,
             double latitude, double longitude);
+
     const std::string &getCode() const;
+
+    const std::string &getName() const;
+
+    const std::string &getCity() const;
+
+    const std::string &getCountry() const;
+
+    double getLatitude() const;
+
+    double getLongitude() const;
+
     bool operator==(const Airport &rhs) const;
 
 private:
@@ -24,6 +34,20 @@ private:
     std::string country;
     double latitude;
     double longitude;
+};
+
+struct airportHash {
+    int operator()(const Airport &airport) const {
+        const std::string& s = airport.getCode();
+        int v = 0;
+        for (char i : s)
+            v = 37*v + i;
+        return v;
+    }
+
+    bool operator()(const Airport &airport1, const Airport &airport2) const {
+        return airport1 == airport2;
+    }
 };
 
 #endif //AIRPLANES_AIRPORT_H
