ea16c89482a48a4efae1329c276436468ce23c97
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2022-12-30 17:18:17 +0000

    airline with getters

--- Airline.cpp
+++ Airline.cpp
@@ -4,18 +4,25 @@
 
 #include "Airline.h"
 
-using namespace std;
-
-Airline::Airline(const string &code) : code(code) {}
-
 Airline::Airline(const std::string &code, const std::string &name, const std::string &callsign,
                  const std::string &country) : code(code), name(name), callsign(callsign), country(country) {}
 
-const string &Airline::getCode() const {
+const std::string &Airline::getCode() const {
     return code;
+}
+
+const std::string &Airline::getName() const {
+    return name;
+}
+
+const std::string &Airline::getCallsign() const {
+    return callsign;
+}
+
+const std::string &Airline::getCountry() const {
+    return country;
 }
 
 bool Airline::operator==(const Airline &rhs) const {
     return code == rhs.code;
 }
-
--- Airline.h
+++ Airline.h
@@ -10,9 +10,16 @@
 
 class Airline {
 public:
-    Airline(const std::string &code);
     Airline(const std::string &code, const std::string &name, const std::string &callsign, const std::string &country);
+
     const std::string &getCode() const;
+
+    const std::string &getName() const;
+
+    const std::string &getCallsign() const;
+
+    const std::string &getCountry() const;
+
     bool operator==(const Airline &rhs) const;
 
 private:
@@ -22,5 +29,18 @@ private:
     std::string country;
 };
 
+struct airlineHash {
+    int operator()(const Airline &airline) const {
+        const std::string& s = airline.getCode();
+        int v = 0;
+        for (char i : s)
+            v = 37*v + i;
+        return v;
+    }
+
+    bool operator()(const Airline &airline1, const Airline &airline2) const {
+        return airline1 == airline2;
+    }
+};
 
 #endif //AIRPLANES_AIRLINE_H
